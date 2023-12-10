4575b659e28c1a3b223f910869f6c7b9162f0cf6
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-12-10 15:11:36 +0700

    charing code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,23 @@
-import express, { Request, Response } from "express";
-import "express-async-errors";
-import { json } from "body-parser";
+import mongoose from 'mongoose';
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-import mongoose from "mongoose";
+import { app } from './app';
 
-const app = express();
-app.use(json());
-
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Hello");
-});
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 const start = async () => {
-  try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/");
-    console.log("Connected to MongoDB");
-    
-  } catch (error) {
-    console.log(error);
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
   }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
 };
 
-app.listen(3000, () => {
-  start();
-  console.log("Listening on port 3000!!!!!!!!");
-});
-// Microservice tickets
+start();
